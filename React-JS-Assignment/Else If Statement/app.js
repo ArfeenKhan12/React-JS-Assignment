b0d5c1670f80e6c814d9ca3f0a16367a10b3c693
Arfeen Khan <155321957+ArfeenKhan12@users.noreply.github.com>
2024-02-29 23:32:22 +0500

    ReactJS Assignment Created

---
+++ React-JS-Assignment/Else If Statement/app.js
@@ -0,0 +1,54 @@
+// IF ELSE STATEMENT STARTED
+
+// QUESION 01
+
+// var number = +prompt('Enter your Number Here!')
+
+// if (number % 3 === 0 ){
+//     console.log('Number is divisible by 3')
+// }else{
+//     console.log('Number is not divisible by 3')
+// }
+
+// QUESTION 02
+
+// var number = +prompt('Enter your Number')
+
+// if (number % 2 === 0 ){
+//     console.log('This is a even number')
+// }else{
+//     console.log('This is a odd number')
+// }
+
+// QUESTION 03
+
+// var age = +prompt('Write you Age')
+
+// if ( age >=18 ){
+//     console.log('Old enough')
+// }else{
+//     console.log('Too young')
+// }
+
+// QUESTION 04
+
+// var userName = prompt('Please, Enter your Name!')
+
+// if (userName == "Arfeen"){
+//     console.log('Hello,Arfeen Its great to see you again!')
+// }else{
+//     console.log('Welcome')
+// }
+
+// QUESTION 05
+
+// var number = +prompt('Enter your Number')
+
+// if (number % 3 === 0 ){
+//     console.log('Number is divisible by 3')
+// }else{
+//     console.log('Number is not divisible by 3')
+// }
+
+
+// IF ELSE STATEMENT ENDED
---
+++ React-JS-Assignment/app.js
@@ -0,0 +1,78 @@
+// chapter 01 started 
+
+// Question 01
+
+// alert('Error! Please enter a valid password.')
+
+// Question 02
+
+// alert('Welcome to JS land \n Happy Coding.')
+
+// Question 03
+
+// alert('Welcome to js Land...')
+
+// Question 04
+
+// var message = 'Hello... I can run JS through my web browers console.';
+// alert(message)
+
+// chapter 01 ended
+
+
+// chapter 02 ended  
+
+//   Question 1&2&3&4  
+// var userName = 'Muhammad Arfeen'
+// var age = '16 years old';
+// var info = 'Certified Web and app development';
+
+// alert(userName);
+// alert(age);
+// alert(info);
+
+//  Question 5  
+// var food = 'PIZZA \n PIZZ \n PIZ \n PI \n P'
+// alert(food)
+
+//  Question 6  
+// var email = 'My email address is xyz@gmail.com'
+// alert(email)
+
+//  Question 7  
+// var book = 'I am trying to learn from the book A smarter \n way to learn javascript'
+// alert(book)
+
+//  Question 8
+// var htmlContent = 'Yah! I can write Html content Throught JavaScipt'
+// alert(htmlContent)
+
+//  Question 9
+
+// var jsBoder = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(jsBoder)
+
+// chapter 02 ended 
+
+//  Chapter 03 started
+
+// Q 1 
+// var age = 16;
+// alert("i am " + age + " years old");
+
+// Q 2 
+// var time = 2;
+// alert("you have visted this site " + time + " times");
+
+// Q 3 
+// var birthYear = 2007;
+// alert("My birth year " + birthYear);
+
+// Q 4
+// var visitorsName = "Muhammad Arfeen";
+// var productTilte = "on XYZ Clothing store"
+// var quantity = " ordered 5 T-shirt(s)"
+
+// alert(visitorsName + '' + quantity + '' + productTilte);
+
+//  Chapter 03 ended
